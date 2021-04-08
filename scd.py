55b4616915ef3b5147ae47d5db593ceff7d65760
genndy007 <kochevgenazp@gmail.com>
2021-04-09 00:12:05 +0300

    Add changing dimensions

---
+++ scd.py
@@ -0,0 +1,26 @@
+import psycopg2
+from db_fill import config
+
+
+def change_fact_venue_tz(venue_id, new_tz_id):
+    sql = """UPDATE outcome_facts SET timezone_id=%s WHERE venue_id=%s"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sql, (new_tz_id, venue_id))
+        print(f'Now venue_id {venue_id} has timezone_id {new_tz_id}')
+
+        conn.commit()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+change_fact_venue_tz(6, 7)
